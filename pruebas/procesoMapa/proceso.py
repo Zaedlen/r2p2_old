3d3d9ca13c5200a916f976bd1d6b870f6bce6e4e
Zaedlen <34587930+Zaedlen@users.noreply.github.com>
2021-09-27 04:59:31 +0200

    pruebas

---
+++ pruebas/procesoMapa/proceso.py
@@ -0,0 +1,260 @@
+# import pygame
+
+# class Fondo(pygame.sprite.Sprite):
+#     def __init__(self, image):
+#         pygame.sprite.Sprite.__init__(self)
+#         self.image = image
+#         # self.image.set_colorkey('#FFFFFF')
+#         self.rect = self.image.get_rect()
+#         # self.mask = pygame.mask.from_surface(self.image )
+
+
+
+# pygame.init()
+# clock = pygame.time.Clock()
+
+# image = pygame.image.load("stage_color.png")
+
+# window = pygame.display.set_mode(image.get_size())
+
+# fondo = Fondo(image)
+
+
+
+# all_sprites = pygame.sprite.Group(fondo)
+
+
+# run = True
+# while run:
+#     clock.tick(60)
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             run = False
+
+#     window.fill('#00FF00')
+#     all_sprites.draw(window)
+
+#     pygame.display.update()
+
+# pygame.quit()
+# exit()
+
+# from PIL import Image
+
+# with Image.open("stage_color.png").convert('P') as aux:
+#     # aux.convert('P')
+#     # print(aux.palette.palette)
+#     im = aux.load()
+#     width, height = aux.size
+
+
+# a = set()
+# for i in range(width):
+#     for j in range(height):
+#         a.add(im[i,j])
+
+# print(a)
+
+
+from PIL import Image
+import numpy as np
+
+# PALETTE = [
+#     0,   0,   0,  # black,  00
+#     0,   255, 0,  # green,  01
+#     255, 0,   0,  # red,    10
+#     255, 255, 0,  # yellow, 11
+# ]
+
+# with Image.open("stage_color.png") as aux:
+#     paleta = Image.new("P", (1, 1), 0)
+#     paleta.putpalette(PALETTE)
+#     aux = aux.convert('RGB')
+#     im = aux.quantize(4, palette=paleta)
+
+# a = set()
+# for i in range(im.width):
+#     for j in range(im.height):
+#         a.add(im.load()[i,j])
+
+# print(a)
+
+# im.save('temp.png')
+
+
+
+PRUEBA = [
+    [
+        [  0,  0,  0],
+        [  0,  0,  0],
+        [  0,  0,  0],
+        [  0,  0,  0]
+    ],
+    [
+        [  0,  0,  0],
+        [255,255,  0],
+        [  0,  0,255],
+        [  0,  0,  0]
+    ],
+    [
+        [  0,  0,  0],
+        [255,  0,  0],
+        [255,255,255],
+        [  0,  0,  0]
+    ],
+    [
+        [  0,  0,  0],
+        [  0,  0,  0],
+        [  0,  0,  0],
+        [  0,  0,  0]
+    ]
+]
+
+# array = np.array(PRUEBA, dtype=np.uint8)
+# res = Image.fromarray(array)
+# res.save("prueba.png")
+
+
+with Image.open("stage_color.png").convert('RGB') as aux:
+# with Image.open("prueba.png").convert('RGB') as aux:
+    array = np.array(aux)
+
+print(array.shape)
+# print(array)
+# print(array.T)
+
+red, green, blue = array.T
+
+objetivo_negro = (red != 0) | (green != 0) | (blue != 0) # En este caso todo lo que no es negro (0, 0, 0)
+objetivo_rojo = (red != 255) | (green != 0) | (blue != 0) # En este caso todo lo que no es rojo (255, 0, 0)
+objetivo_amarillo = (red != 255) | (green != 255) | (blue != 0) # En este caso todo lo que no es amarillo (255, 255, 0)
+objetivo_azul = (red != 0) | (green != 0) | (blue != 255) # En este caso todo lo que no es azul (0, 0, 255)
+# print(objetivo_negro)
+negro = array.copy()
+rojo = array.copy()
+amarillo = array.copy()
+azul = array.copy()
+
+negro[objetivo_negro.T] = (255, 255, 255) # Si objetivo_negro es todo lo que no es negro; pues coloreamos todo lo que no es negro de blanco
+rojo[objetivo_rojo.T] = (255, 255, 255)
+amarillo[objetivo_amarillo.T] = (255, 255, 255)
+azul[objetivo_azul.T] = (255, 255, 255)
+
+# print(red)
+
+# print(array[...,:-1])
+
+# array[array != 0] = 100
+# print(array)
+
+
+# transformador = np.zeros([256,3], dtype=np.uint8)
+# # print(transformador)
+# # transformador[255] = 255
+# # transformador[129] = 255
+
+# pixels = transformador[array]
+# print(pixels.shape)
+# # print(pixels)
+
+
+resultado = Image.fromarray(negro)
+resultado.save('negro.png')
+resultado = Image.fromarray(rojo)
+resultado.save('rojo.png')
+resultado = Image.fromarray(amarillo)
+resultado.save('amarillo.png')
+resultado = Image.fromarray(azul)
+resultado.save('azul.png')
+
+
+import pygame, random
+
+class SpriteObject(pygame.sprite.Sprite):
+    def __init__(self, x, y, w, h, color):
+        pygame.sprite.Sprite.__init__(self)
+        self.angle = random.randrange(360)
+        self.original_image = pygame.Surface([w, h], pygame.SRCALPHA)
+        self.original_image.fill(color)
+        self.image = self.original_image
+        self.rect = self.image.get_rect(center = (x, y))
+        self.mask = pygame.mask.from_surface(self.image )
+    def update(self):
+        self.rotate()
+    def rotate(self):
+        self.angle += 0.3
+        self.image = pygame.transform.rotate(self.original_image, self.angle)
+        self.rect = self.image.get_rect(center = self.rect.center)
+        self.mask = pygame.mask.from_surface(self.image )
+
+class Fondo(pygame.sprite.Sprite):
+    def __init__(self, image):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.image.set_colorkey('#FFFFFF')
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+
+
+
+pygame.init()
+clock = pygame.time.Clock()
+
+image = pygame.image.load("negro.png")
+
+window = pygame.display.set_mode(image.get_size())
+
+moving_object = SpriteObject(0, 0, 50, 50, (128, 0, 255))
+
+negro = Fondo(image)
+image = pygame.image.load("rojo.png")
+rojo = Fondo(image)
+image = pygame.image.load("amarillo.png")
+amarillo = Fondo(image)
+image = pygame.image.load("azul.png")
+azul = Fondo(image)
+# fondo.rect = pygame.Rect(window.get_rect())
+
+moving = pygame.sprite.Group(moving_object)
+sprite_negro = pygame.sprite.Group(negro)
+sprite_rojo = pygame.sprite.Group(rojo)
+sprite_amarillo = pygame.sprite.Group(amarillo)
+sprite_azul = pygame.sprite.Group(azul)
+
+run = True
+while run:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    moving_object.rect.center = pygame.mouse.get_pos()
+    moving.update()
+    sprite_negro.update()
+    sprite_rojo.update()
+    sprite_amarillo.update()
+    sprite_azul.update() 
+    collide_negro = pygame.sprite.spritecollide(moving_object, sprite_negro, False, pygame.sprite.collide_mask)
+    collide_rojo = pygame.sprite.spritecollide(moving_object, sprite_rojo, False, pygame.sprite.collide_mask)
+    collide_amarillo = pygame.sprite.spritecollide(moving_object, sprite_amarillo, False, pygame.sprite.collide_mask)
+    collide_azul = pygame.sprite.spritecollide(moving_object, sprite_azul, False, pygame.sprite.collide_mask)
+    
+    # window.fill((255, 0, 0) if collide else (255, 255, 255))
+    if collide_negro: window.fill('purple')
+    elif collide_rojo: window.fill('red')
+    elif collide_amarillo: window.fill('yellow')
+    elif collide_azul: window.fill('blue')
+    else: window.fill('white')
+    # if collide: print('Colision')
+    # all_sprites.draw(window)
+    sprite_negro.draw(window)
+    sprite_rojo.draw(window)
+    sprite_amarillo.draw(window)
+    sprite_azul.draw(window)
+    moving.draw(window)
+    # fondo.draw(window)
+    # moving_object.draw(window)
+    pygame.display.update()
+
+pygame.quit()
+exit()
--- pruebas/pruebas_pygame.py
+++ pruebas/pruebas_pygame.py
@@ -1,39 +1,193 @@
-import pygame
-import sys
-from pygame.locals import *
+# import pygame
+# import sys
+# from pygame.locals import *
+
+# pygame.init()
+
+# FPS = 30
+# FramesPerSecond = pygame.time.Clock()
+
+# BLACK = pygame.Color(0,0,0)
+# WHITE = (255,255,255)
+# BLUE = (0,0,255)
+# RED = (255,0,0)
+# GREEN = (0,255,0)
+
+# DISPLAYSURF = pygame.display.set_mode((300,300))
+# DISPLAYSURF.fill(WHITE)
+# pygame.display.set_caption("Example")
+
+# pygame.draw.line(DISPLAYSURF, BLUE, (150, 130), (130, 170))
+# pygame.draw.line(DISPLAYSURF, BLUE, (150, 130), (170, 170))
+# pygame.draw.line(DISPLAYSURF, GREEN, (130, 170), (170, 170))
+# pygame.draw.circle(DISPLAYSURF, BLACK, (100, 50), 30)
+# pygame.draw.circle(DISPLAYSURF, BLACK, (200, 50), 30)
+# pygame.draw.rect(DISPLAYSURF, RED, (100, 200, 100, 50), 2)
+# pygame.draw.rect(DISPLAYSURF, BLACK, (110, 260, 80, 5))
+
+# # Game Loop Begin
+# while True:
+#     pygame.display.update()
+#     for event in pygame.event.get():
+#         if event.type == QUIT:
+#             pygame.quit()
+#             sys.exit()
+
+#     FramesPerSecond.tick(FPS)
+
+
+
+# import pygame
+
+# pygame.init()
+# window = pygame.display.set_mode((250, 250))
+
+# sprite1 = pygame.sprite.Sprite()
+# sprite1.image = pygame.Surface((80, 80), pygame.SRCALPHA)
+# pygame.draw.circle(sprite1.image, (255, 0, 0), (40, 40), 40)
+# sprite1.rect = pygame.Rect(*window.get_rect().center, 0, 0).inflate(40, 40)
+# sprite2 = pygame.sprite.Sprite()
+# sprite2.image = pygame.Surface((80, 89), pygame.SRCALPHA)
+# pygame.draw.circle(sprite2.image, (0, 255, 0), (40, 40), 40)
+# sprite2.rect = pygame.Rect(*window.get_rect().center, 0, 0).inflate(80, 80)
+
+# all_group = pygame.sprite.Group([sprite2, sprite1])
+# test_group = pygame.sprite.Group(sprite2)
+
+# run = True
+# while run:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             run = False
+
+#     sprite1.rect.center = pygame.mouse.get_pos()
+#     collide = pygame.sprite.spritecollide(sprite1, test_group, False, pygame.sprite.collide_circle)
+
+#     window.fill(0)
+
+#     pygame.draw.rect(window, (0,0,255), sprite2.rect)
+    
+#     all_group.draw(window)
+#     for s in collide:
+#         pygame.draw.circle(window, (255, 255, 255), s.rect.center, s.rect.width // 2, 5)
+
+#     pygame.draw.rect(window, (0,0,255), sprite1.rect)
+
+#     pygame.display.flip()
+
+# pygame.quit()
+# exit()
+
+
+
+
+# import pygame, random
+
+# class SpriteObject(pygame.sprite.Sprite):
+#     def __init__(self, x, y, w, h, color):
+#         pygame.sprite.Sprite.__init__(self)
+#         self.angle = random.randrange(360)
+#         self.original_image = pygame.Surface([w, h], pygame.SRCALPHA)
+#         self.original_image.fill(color)
+#         self.image = self.original_image
+#         self.rect = self.image.get_rect(center = (x, y))
+#         self.mask = pygame.mask.from_surface(self.image )
+#     def update(self):
+#         self.rotate()
+#     def rotate(self):
+#         self.angle += 0.3
+#         self.image = pygame.transform.rotate(self.original_image, self.angle)
+#         self.rect = self.image.get_rect(center = self.rect.center)
+#         self.mask = pygame.mask.from_surface(self.image )
+
+# pygame.init()
+# clock = pygame.time.Clock()
+# window = pygame.display.set_mode((400, 400))
+# size = window.get_size()
+
+# moving_object = SpriteObject(0, 0, 50, 50, (128, 0, 255))
+# static_objects = [
+#     SpriteObject(size[0] // 2, size[1] // 3, 100, 50, (128, 128, 128)),
+#     SpriteObject(size[0] // 4, size[1] * 2 // 3, 100, 50, (128, 128, 128)),
+#     SpriteObject(size[0] * 3 // 4, size[1] * 2 // 3, 100, 50, (128, 128, 128))
+# ]
+# all_sprites = pygame.sprite.Group([moving_object] + static_objects)
+# static_sprites = pygame.sprite.Group(static_objects)
+
+# run = True
+# while run:
+#     clock.tick(60)
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             run = False
+
+#     moving_object.rect.center = pygame.mouse.get_pos()
+#     all_sprites.update() 
+#     collide = pygame.sprite.spritecollide(moving_object, static_sprites, False, pygame.sprite.collide_mask)
+    
+#     window.fill((255, 0, 0) if collide else (255, 255, 255))
+#     all_sprites.draw(window)
+#     pygame.display.update()
+
+# pygame.quit()
+# exit()
+
+
+
+
+import pygame, random
+
+class SpriteObject(pygame.sprite.Sprite):
+    def __init__(self, x, y, w, h, color):
+        pygame.sprite.Sprite.__init__(self)
+        self.angle = random.randrange(360)
+        self.original_image = pygame.Surface([w, h], pygame.SRCALPHA)
+        self.original_image.fill(color)
+        self.image = self.original_image
+        self.rect = self.image.get_rect(center = (x, y))
+        self.mask = pygame.mask.from_surface(self.image )
+    def update(self):
+        self.rotate()
+    def rotate(self):
+        self.angle += 0.3
+        self.image = pygame.transform.rotate(self.original_image, self.angle)
+        self.rect = self.image.get_rect(center = self.rect.center)
+        self.mask = pygame.mask.from_surface(self.image )
+
+class Fondo(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("../res/stage.png")
+        self.mask = pygame.mask.from_surface(self.image )
 
 pygame.init()
+clock = pygame.time.Clock()
+window = pygame.display.set_mode((400, 400))
+size = window.get_size()
 
-FPS = 30
-FramesPerSecond = pygame.time.Clock()
+moving_object = SpriteObject(0, 0, 50, 50, (128, 0, 255))
+fondo = Fondo()
+fondo.rect = pygame.Rect(window.get_rect())
+all_sprites = pygame.sprite.Group([fondo, moving_object])
+static_sprites = pygame.sprite.Group([fondo])
 
-BLACK = pygame.Color(0,0,0)
-WHITE = (255,255,255)
-BLUE = (0,0,255)
-RED = (255,0,0)
-GREEN = (0,255,0)
-
-DISPLAYSURF = pygame.display.set_mode((300,300))
-DISPLAYSURF.fill(WHITE)
-pygame.display.set_caption("Example")
-
-pygame.draw.line(DISPLAYSURF, BLUE, (150, 130), (130, 170))
-pygame.draw.line(DISPLAYSURF, BLUE, (150, 130), (170, 170))
-pygame.draw.line(DISPLAYSURF, GREEN, (130, 170), (170, 170))
-pygame.draw.circle(DISPLAYSURF, BLACK, (100, 50), 30)
-pygame.draw.circle(DISPLAYSURF, BLACK, (200, 50), 30)
-pygame.draw.rect(DISPLAYSURF, RED, (100, 200, 100, 50), 2)
-pygame.draw.rect(DISPLAYSURF, BLACK, (110, 260, 80, 5))
-
-# Game Loop Begin
-while True:
-    pygame.display.update()
+run = True
+while run:
+    clock.tick(60)
     for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+        if event.type == pygame.QUIT:
+            run = False
 
-    FramesPerSecond.tick(FPS)
+    moving_object.rect.center = pygame.mouse.get_pos()
+    all_sprites.update() 
+    collide = pygame.sprite.spritecollide(moving_object, static_sprites, False, pygame.sprite.collide_mask)
+    
+    window.fill((255, 0, 0) if collide else (255, 255, 255))
+    # if collide: print('Colision')
+    all_sprites.draw(window)
+    # fondo.draw(window)
+    # moving_object.draw(window)
+    pygame.display.update()
 
-
-
+pygame.quit()
+exit()

5e815fe9711a9fc612265f78efbd6c276261b002
Zaedlen <34587930+Zaedlen@users.noreply.github.com>
2021-10-06 01:38:35 +0200

    pruebas

--- pruebas/procesoMapa/proceso.py
+++ pruebas/procesoMapa/proceso.py
@@ -263,22 +263,49 @@
 # ----------------------------------------------------------------------------------------------------------------------
 
 
+
 from PIL import Image
 import numpy as np
 import math
 import time
 
+from numpy.core.fromnumeric import shape
 
-def pintarLinea(angle, ruta="lienzo.png", save="lienzo_pintado.png", x0=250, y0=250, large=50, thickness=5, color = (0,0,0)):
+# Funciones auxiliares =================================================================================================
+def cargarArray(ruta:str):
     with Image.open(ruta).convert('RGB') as aux:
         array = np.array(aux)
 
+    return array
+
+
+def cargarArray_pygame(ruta:str):
+    array = cargarArray(ruta)
     # print(array.shape)
     array = np.rot90(array)
     array:np.ndarray = np.flipud(array)
-    shape = array.shape
     # print(array.dtype)
     # print(array.shape)
+    return array
+
+
+def guardarArray(array:np.ndarray, ruta:str):
+    resultado = Image.fromarray(array)
+    resultado.save(ruta)
+
+
+def guardarArray_pygame(array:np.ndarray, ruta:str):
+    array = np.rot90(array)
+    array:np.ndarray = np.flipud(array)
+
+    guardarArray(array, ruta)
+
+
+# PINTAR LINEA =========================================================================================================
+def pintarLinea(angle, ruta="lienzo.png", save="lienzo_pintado.png", x0=250, y0=250, large=50, thickness=5, color = (0,0,0)):
+    array = cargarArray_pygame(ruta)
+    shape = array.shape
+    
     # print(array[0,0]) # pygame = (255,0,0)
     # print(array[0,-1]) # pygame = (0,0,0)
     # print(array[-1,0]) # pygame = (0,0,255)
@@ -292,7 +319,7 @@ def pintarLinea(angle, ruta="lienzo.png", save="lienzo_pintado.png", x0=250, y0=
 
     start = time.time()
 
-    # Mario & me´s algorithm =========================================================================================
+    # Mario & me´s algorithm -------------------------------------------------------------------------------------------
     perpend = math.radians(angle+90)
     angle = math.radians(angle)
     perpend_cos = math.cos(perpend)
@@ -316,7 +343,7 @@ def pintarLinea(angle, ruta="lienzo.png", save="lienzo_pintado.png", x0=250, y0=
 
 
 
-    # # Bresenham's line algorithm ============================================================= no me convence ========
+    # # Bresenham's line algorithm ------------------------------------------------------------- no me convence --------
     # x1:int = round(x0 + (math.cos(angle) * large))
     # y1:int = round(y0 + (math.sin(angle) * large))
 
@@ -343,7 +370,7 @@ def pintarLinea(angle, ruta="lienzo.png", save="lienzo_pintado.png", x0=250, y0=
      
 
 
-    # # Digital Differential Analyzer algorithm =============================================== no me convence =========
+    # # Digital Differential Analyzer algorithm ----------------------------------------------- no me convence ---------
     # x1 = x0 + (math.cos(angle) * large)
     # y1 = y0 + (math.sin(angle) * large)
 
@@ -380,31 +407,209 @@ def pintarLinea(angle, ruta="lienzo.png", save="lienzo_pintado.png", x0=250, y0=
     stop = time.time()
     print('Time:', stop-start,'s')
 
-    array = np.rot90(array)
-    array:np.ndarray = np.flipud(array)
-    
-
-    resultado = Image.fromarray(array)
-    resultado.save(save)
+    guardarArray_pygame(array, save)
 
     return (stop - start)
 
 
-# pintarLinea(0)
-# pintarLinea(90,ruta="lienzo_pintado.png", save="lienzo_pintado.png", color=(255,0,0))
-# pintarLinea(180,ruta="lienzo_pintado.png", save="lienzo_pintado.png", color=(0,255,0))
-# pintarLinea(270,ruta="lienzo_pintado.png", save="lienzo_pintado.png", color=(0,0,255))
-# pintarLinea(45,ruta="lienzo_pintado.png", save="lienzo_pintado.png", color=(255,255,0))
+# PRUEBAS PINTAR LINEA =================================================================================================
 
-inicio = time.time()
+# # pintarLinea(0)
+# # pintarLinea(90,ruta="lienzo_pintado.png", save="lienzo_pintado.png", color=(255,0,0))
+# # pintarLinea(180,ruta="lienzo_pintado.png", save="lienzo_pintado.png", color=(0,255,0))
+# # pintarLinea(270,ruta="lienzo_pintado.png", save="lienzo_pintado.png", color=(0,0,255))
+# # pintarLinea(45,ruta="lienzo_pintado.png", save="lienzo_pintado.png", color=(255,255,0))
 
-pintarLinea(0, color=(255,255,255))
-count = 0
-for i in range(0,360,360//60):
-    # count += pintarLinea(i,ruta="lienzo_pintado.png", save="lienzo_pintado.png", thickness=40, large=450, color=(round((i/360)*255),round((1-(i/360))*255),round((i/360)*255)))
-    count += pintarLinea(i,ruta="lienzo_pintado.png", save="lienzo_pintado.png", thickness=1, large=200)
+# inicio = time.time()
 
-print('Total time:',count,'s')
+# pintarLinea(0, color=(255,255,255))
+# count = 0
+# for i in range(0,360,360//60):
+#     # count += pintarLinea(i,ruta="lienzo_pintado.png", save="lienzo_pintado.png", thickness=40, large=450, color=(round((i/360)*255),round((1-(i/360))*255),round((i/360)*255)))
+#     count += pintarLinea(i,ruta="lienzo_pintado.png", save="lienzo_pintado.png", thickness=1, large=200)
 
-fin = time.time()
-print('Run time:',fin-inicio,'s')
+# print('Total time:',count,'s')
+
+# fin = time.time()
+# print('Run time:',fin-inicio,'s')
+
+
+
+# ARRAY DE TUPLAS ======================================================================================================
+def arrayTuplas(carga:str, guardado:str):
+    with Image.open(carga).convert('RGB') as aux:
+        array = np.array(aux)
+
+    array = np.rot90(array)
+    array:np.ndarray = np.flipud(array)
+
+    # a = np.zeros((array.shape[0],array.shape[1]),dtype=[('R', 'u1'),('G', 'u1'),('B', 'u1')])
+    a = np.zeros((array.shape[0],array.shape[1]),dtype=tuple)
+    for i in range(array.shape[0]):
+        for j in range(array.shape[1]):
+            a[i,j] = (array[i,j,0], array[i,j,1], array[i,j,2])
+    array = a
+    print(type(array[0,0]))
+    
+    # a[0,0] = (255,255,255)
+    # a[1,0][0] = 126
+    # print(a)
+    # print(a['R'])
+
+    # print(array.T.shape)
+    # array = array.T
+    # array = np.array(array, dtype=dt)
+    # print(array.shape)
+    # print(array[0,0])
+
+    # array.resize(400,600)
+    # print(array.shape)
+
+    # array = np.rot90(array)
+    # array:np.ndarray = np.flipud(array)
+    # print(array.shape)
+
+    print(array[0,0]) # pygame = (255,0,0)
+    print(array[0,-1]) # pygame = (0,0,0)
+    print(array[-1,0]) # pygame = (0,0,255)
+    print(array[-1,-1]) # pygame = (0,255,0)
+
+    # lista = [[y == (255,0,0) for y in x] for x in array]
+    # array[lista] = np.array((255,0,255), dtype=tuple)
+    # print(array[0,0])
+    aux = np.empty(1,dtype=object)
+    aux[:] = [(255,0,0)]
+    aux2 = np.empty(1,dtype=object); aux2[0] = (255,0,255)
+    print(aux2[0])
+    array[array == aux] = aux2
+    print(array)
+
+# PRUEBAS ARRAY DE TUPLAS ==============================================================================================
+# arrayTuplas("lienzo.png", "lienzo_pintado.png")
+
+
+
+# Procesar imagen para obtener objetos =================================================================================
+import sys
+loggedData = ""
+
+def loggear(*args):
+    global loggedData
+    for a in args:
+        loggedData += str(a)
+    loggedData += '\n'
+
+class Objeto():
+    def __init__(self, initialPixel, id=0) -> None:
+        self.pixeles = []
+        self.pixeles.append(initialPixel)
+        self.id = id
+        loggear('      Pixel', initialPixel, 'added to Objecto', self.id)
+
+    def addPixel(self, pixel:tuple):
+        self.pixeles.append(pixel)
+
+    def isIn(self, pixel:tuple):
+        return pixel in self.pixeles
+
+    def canBeAdded(self, pixel, image:np.ndarray):
+        preX = (pixel[0] - 1) if (pixel[0] != 0) else pixel[0]
+        posX = (pixel[0] + 1) if (pixel[0] != (image.shape[0]-1)) else pixel[0]
+        preY = (pixel[1] - 1) if (pixel[1] != 0) else pixel[1]
+        posY = (pixel[1] + 1) if (pixel[1] != (image.shape[1]-1)) else pixel[1]
+        for i in range(preX, posX+1):
+            for j in range(preY, posY+1):
+                if ((i,j) in self.pixeles) and (pixel not in self.pixeles): return True
+        return False
+
+    def addPixel(self, pixel, image:np.ndarray):
+        if self.canBeAdded(pixel, image):
+            self.pixeles.append(pixel)
+            loggear('      Pixel', pixel, 'added to Objecto', self.id)
+            return True
+        else:
+            return False
+
+    def getCoord(self):
+        coordX = []
+        coordY = []
+        for p in self.pixeles:
+            coordX.append(p[0])
+            coordY.append(p[1])
+        return coordX, coordY
+
+class TipoColision():
+    def __init__(self, color:tuple, pixel=None) -> None:
+        self.objetos = []
+        if pixel:
+            self.objetos.append(Objeto(pixel))
+        self.color = color
+        loggear('   Created new type:', self.color)
+
+    # def addTo
+    def addPixel(self, pixel, color:tuple, image:np.ndarray):
+        if self.color == color:
+            added = False
+            for obj in self.objetos:
+                obj:Objeto
+                if obj.addPixel(pixel, image):
+                    added = True
+                    loggear('   Type:', self.color, ' Added pixel', pixel, 'to existing Objeto', obj.id)
+                    break
+            if not added:
+                self.objetos.append(Objeto(pixel, len(self.objetos)))
+                loggear('   Type:', self.color, ' Added pixel', pixel, 'to new Objeto', len(self.objetos))
+            return True
+        else:
+            return False
+
+
+
+def procesarImagen(carga, guardado):
+    array = cargarArray_pygame(carga)
+    colisiones = []
+    for i in range(0, array.shape[0]):
+        for j in range(0, array.shape[1]):
+            if tuple(array[i,j]) != (255,255,255):
+                added = False
+                for tipo in colisiones:
+                    tipo:TipoColision
+                    if tipo.addPixel((i,j), tuple(array[i,j]), array):
+                        added = True
+                        loggear('Added Pixel', (i,j), 'to existing Tipo', tipo.color)
+                        break
+                if not added:
+                    colisiones.append(TipoColision(tuple(array[i,j]), (i,j)))
+                    loggear('Added Pixel', (i,j), 'to new Tipo', tuple(array[i,j]))
+    
+
+    # print(array[(0,0)])
+    # print(array[(-1,-1)])
+    # print(array[[0,-1],[0,-1]])
+    
+    objetos = []
+    for tipo in colisiones:
+        tipo:TipoColision
+        
+        objetos.extend(tipo.objetos)
+    division = 255 // len(objetos)
+    for i in range(len(objetos)):
+        x,y = objetos[i].getCoord()
+        array[x,y] = (division*i, division*i, division*i)
+
+    guardarArray_pygame(array, guardado)
+
+    return colisiones
+            
+                        
+
+colisiones = procesarImagen('lienzo_obstaculos.png', 'lienzo_pintado.png')
+print(len(colisiones))
+
+with open('logs.txt', 'w') as logg:
+    print(loggedData, file=logg)
+
+
+
+
+
